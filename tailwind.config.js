556fa4c6fba775b4d060bd43c555d288e588194a
Justine <justinemarie.altar-18@cpu.edu.ph>
2024-10-24 14:21:03 +0800

    [Justine] Input and Add button

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,7 @@ module.exports = {
       colors: {
         customBackground: '#282B37', 
         customInputBkg: '#404A5B',
+        customAddBtnBkg: '#2C3D5B',
         whiteTransparent: '#FFFFFF99',
         whiteTransparent2: '#FFFFFF80',
       },
@@ -16,3 +17,4 @@ module.exports = {
   },
   plugins: [],
 }
+
